60500115999b1781e71345f1a7f203e4dda66ece
ahow6552 <a0961500945@gmail.com>
2024-10-20 18:15:49 +0800

    update module usage

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-"""
+the project based on https://github.com/mkkellogg/GaussianSplats3D
+```
 python3 manage.py migrate 
 python3 manage.py runserver
-"""
+```
--- project/settings.py
+++ project/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
